bfe892a2ab5c65ac70d9c3f08db03d83d4b126a5
mengestu <44705427+mengestu@users.noreply.github.com>
2021-04-21 14:19:47 +0300

    Added Pages and Routing

---
+++ pages/events/[slug].js
@@ -0,0 +1,8 @@
+export default function EventPage() {
+    return (
+
+        <div>
+        <h1>Event Page</h1>    
+        </div>
+    )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddEvent() {
+    return (
+        <div>
+            <h1>Add Eveent Page</h1>
+        </div>
+    )
+}
